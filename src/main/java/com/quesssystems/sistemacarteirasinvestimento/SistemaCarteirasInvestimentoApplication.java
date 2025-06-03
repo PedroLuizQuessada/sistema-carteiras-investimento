33000ed81a9d9a4cf953470cce404bb3074637a8
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-06-03 11:38:07 -0300

    criando projeto

---
+++ src/main/java/com/quesssystems/sistemacarteirasinvestimento/SistemaCarteirasInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.quesssystems.sistemacarteirasinvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCarteirasInvestimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCarteirasInvestimentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quesssystems/sistemacarteirasinvestimento/SistemaCarteirasInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quesssystems.sistemacarteirasinvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCarteirasInvestimentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
