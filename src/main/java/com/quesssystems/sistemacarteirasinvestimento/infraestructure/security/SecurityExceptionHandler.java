6c02cfa7c1711c161fbe7a2961db262bee690e12
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-06-10 21:40:42 -0300

    feat: personalizando recurso não encontrado

--- src/main/java/com/quesssystems/sistemacarteirasinvestimento/infraestructure/security/SecurityExceptionHandler.java
+++ src/main/java/com/quesssystems/sistemacarteirasinvestimento/infraestructure/security/SecurityExceptionHandler.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,5 +16,14 @@ public class SecurityExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleRuntime(RuntimeException ex) {
         log.error(ex.getMessage(), ex);
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String mensagem = "Recurso não encontrado";
+        log.error(mensagem);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, mensagem));
     }
 }
